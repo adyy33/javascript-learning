108a63b3f8fe7f134cfe64c4a962e1c6a31a440a
Aditya Dapale <adityadapale1@gmail.com>
2026-01-14 09:47:42 +0530

    Added js operators examples

---
+++ operators.js
@@ -0,0 +1,8 @@
+let a=5;
+let b="5";
+console.log("addition of a and b is " + (a+b));//it gives '55' because js consider 'a' as string because of 'b' is string
+console.log("subtraction of a and b is " + (a-b));
+console.log("multiplication of a and b is"+(a*b));
+console.log("division of a and b is " + (a/b));
+console.log("a==b is " + (a==b));
+console.log("a===b is " + (a===b));
